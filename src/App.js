2a83c50e35b77a02e5b2a71de3cf3f105f879fe9
Ana Edwards <edwards.ana81@gmail.com>
2022-06-28 12:50:19 +0100

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
+import React, {Suspense} from 'react';
 import {Switch, Redirect, Route} from 'react-router-dom';
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+// import AllQuotes from './pages/AllQuotes';
+// import QuoteDetail from './pages/QuoteDetail';
+// import NewQuote from './pages/NewQuote';
+// import NotFound from './pages/NotFound';
+
+//LAZY LOADING
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes'/>
-        </Route>
+      <Suspense fallback={<div className='centered'>
+        <LoadingSpinner />
+        </div>}
+      >
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes'/>
+          </Route>
 
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes />
+          </Route>
 
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail/>
-        </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail/>
+          </Route>
 
-        <Route path='/new-quote'>
-          <NewQuote/>
-        </Route>
+          <Route path='/new-quote'>
+            <NewQuote/>
+          </Route>
 
-        <Route path='*'>
-          <NotFound/>
-        </Route>
+          <Route path='*'>
+            <NotFound/>
+          </Route>
 
-      </Switch>
+        </Switch>
+      </Suspense>
     </Layout>
     
   );
